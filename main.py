99b45e2fbdfd94b7de92f0bfa338dc54bdd8b398
片山　朋和 <poko.peko.tomo@icloud.com>
2022-03-05 11:49:49 +0900

    update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ pandas.options.display.max_rows = None
 pandas.options.display.max_columns = None
 pandas.options.display.width = 6000
 pandas.options.display.max_colwidth = 6000
+pandas.options.display.colheader_justify = 'left'
 
 API_KEY = os.getenv('API_KEY', default='')
 
@@ -25,7 +26,7 @@ youtube = build(
     'v3',
     developerKey=API_KEY
 )
-playlistKey = 'PL0DCF7F78614F3AE6'
+playlistKey = 'PLAAEA82D2950BC77D'
 
 channelId = youtube.playlistItems().list(part='snippet',
                                          fields='items/snippet/channelId',
@@ -33,10 +34,13 @@ channelId = youtube.playlistItems().list(part='snippet',
                                          maxResults=1).execute()['items'][0]['snippet']['channelId']
 print(channelId)
 channelInfo = youtube.channels().list(
-    part='id,snippet,brandingSettings,contentDetails,statistics,topicDetails',
+    part='snippet,brandingSettings,statistics',
+    fields='items(snippet(thumbnails/high,title),'
+           'brandingSettings/image,'
+           'statistics(subscriberCount,videoCount,viewCount))',
     id=channelId).execute()
 pprint(channelInfo)
-exit(0)
+# exit(0)
 nextPageToken = ''
 videoId = []
 while True:
@@ -57,33 +61,52 @@ while True:
     # time.sleep(3)
 
 # pprint(videoId)
+current_data = []
 for Id in videoId:
     video_info = youtube.videos().list(part='statistics,snippet',
-                                       fields='items(snippet(title,thumbnails(high,maxres)),statistics/viewCount)',
+                                       fields='items(snippet(title,thumbnails(high,maxres)),'
+                                              'statistics/viewCount)',
                                        id=Id).execute()
     video_info = json.loads(unicodedata.normalize('NFKC', json.dumps(video_info)))['items']
     # print(video_info)
     # pprint(video_info)
     if 'maxres' in video_info[0]['snippet']['thumbnails']:
         thumb = video_info[0]['snippet']['thumbnails']['maxres']
-        print('maxres')
+        # print('maxres')
     else:
         thumb = video_info[0]['snippet']['thumbnails']['high']
-        print('high')
+        # print('high')
     # pprint(video_info[0]["snippet"]["thumbnails"])
     data = [[thumb['url'], thumb['width'], thumb['height']],
             unicodedata.normalize('NFKC', video_info[0]['snippet']['title']),
             video_info[0]['statistics']['viewCount'],
             'https://youtu.be/' + Id]
-    pprint(data)
+    # pprint(data)
+    current_data.append(data)
 
-print((datetime.date.today() - datetime.date(2022, 1, 1)).days)
+pprint(current_data)
 
-metadata = {
-    'channel': ''
-}
-get_dict = {
+today = str(datetime.date.today())
+dataframe = pandas.DataFrame([0])
+if 'タイトル' not in dataframe.columns:
+    dataframe['タイトル'] = ''
+if today not in dataframe.columns:
+    dataframe[today] = 0
 
-}
+for data in current_data:
+    url = str(data[3])
+    if data[3] not in dataframe.index.tolist():
+        dataframe.loc[url] = 0
+    # print(dataframe)
+    dataframe.at[url, 'タイトル'] = data[1]
+    dataframe.at[url, today] = int(data[2])
+
+dataframe.drop(0, axis=0, inplace=True)
+dataframe.drop(0, axis=1, inplace=True)
+
+print(dataframe)
+print((datetime.date.today() - datetime.date(2021, 1, 1)).days)
+
+dataframe.to_excel('text.xlsx', sheet_name=channelInfo['items'][0]['snippet']['title'])
 
 wb = openpyxl.Workbook()
