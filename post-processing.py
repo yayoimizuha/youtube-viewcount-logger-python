db72078f2541b87834f8e3e8b16645abd5691d8b
片山　朋和 <poko.peko.tomo@icloud.com>
2022-03-09 23:30:23 +0900

    update

--- post-processing.py
+++ post-processing.py
@@ -1,16 +1,30 @@
 import datetime
+import json
 import os
+import firebase_admin
+from firebase_admin import credentials, firestore, storage
 
 from github import Github
 
+credential = credentials.Certificate(json.loads(os.environ['FIREBASE_CREDENTIAL']))
+firebase_admin.initialize_app(credential, {'storageBucket': 'gs://viewcount-logger-20043.appspot.com/'})
+bucket = storage.bucket()
+blob = bucket.blob(str(datetime.datetime.now()) + '_log.txt')
+blob_url = ''
 with open('out.txt', mode='r') as f:
     file = f.read()
+    blob.upload_from_file(file_obj=file, content_type='text/plain')
+    blob.make_public()
+    blob_url = blob.public_url
+    print(blob_url)
 
 body_text = """{date}
+[{url}]({url})
 ```
 {content}
 ```""".format(date=str(datetime.datetime.now().isoformat()),
-              content=file[len(file) - 30000:-1])
+              content=file[len(file) - 30000:-1],
+              url=blob_url)
 
 gh = Github(os.environ['GITHUB_TOKEN'])
 repo = gh.get_repo('yayoimizuha/youtube-viewcount-logger-python')
