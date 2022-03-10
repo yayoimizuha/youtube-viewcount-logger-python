61024996749ad06a8fb24ac9936fd1ca69f1c2f2
yayoimizuha <poko.peko.tomo@icloud.com>
2022-03-11 02:34:02 +0900

    fix

--- post-processing.py
+++ post-processing.py
@@ -1,21 +1,9 @@
 import datetime
-import json
 import os
-import firebase_admin
-from firebase_admin import credentials, storage
-
 from github import Github
+import firebase_storage_upload
 
-credential = credentials.Certificate(json.loads(os.environ['FIREBASE_CREDENTIAL']))
-firebase_admin.initialize_app(credential, {'storageBucket': 'viewcount-logger-20043.appspot.com'})
-bucket = storage.bucket()
-blob = bucket.blob(blob_name=str(str(datetime.datetime.now()) + '_log.txt'))
-blob_url = ''
-with open('out.txt', mode='rb') as f:
-    blob.upload_from_file(file_obj=f, content_type='text/plain')
-    blob.make_public()
-    blob_url = blob.public_url
-    print(blob_url)
+blob_url = firebase_storage_upload.upload_log()
 
 with open('out.txt', mode='r') as f:
     file = f.read()
