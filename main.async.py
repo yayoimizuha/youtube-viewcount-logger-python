33450cbab55f330523d1c1baf13be663f8bce00a
yayoimizuha <poko.peko.tomo@icloud.com>
2022-10-31 21:02:36 +0900

    update

--- main.async.py
+++ main.async.py
@@ -2,20 +2,21 @@ import asyncio
 import os
 import pprint
 import time
-from aiogoogle import Aiogoogle, auth
+from aiogoogle import Aiogoogle, auth, GoogleAPI
 import const
 
 
-async def google_api():
+async def main():
     async with Aiogoogle(api_key=auth.creds.ApiKey(os.environ["YTV3_API_KEY"])) as aiogoogle:
-        youtube_v3 = await aiogoogle.discover(api_name="youtube", api_version="v3")
+        youtube_v3: GoogleAPI = await aiogoogle.discover(api_name="youtube", api_version="v3")
         all_musics = await asyncio.gather(
-            *[youtube_data_api_v3(playlist_key=playlist_key, group=group, build=youtube_v3, aio=aiogoogle) for
-              playlist_key, group, _ in const.playlists()])
-        pprint.pprint(all_musics)
+            *[list_playlist_content(playlist_key=playlist_key, group=group, build=youtube_v3, aio=aiogoogle)
+              for playlist_key, group, _ in const.playlists()], return_exceptions=True)
+        return all_musics
 
 
-async def youtube_data_api_v3(playlist_key: str, group: str, build: Aiogoogle.discover, aio: Aiogoogle):
+async def list_playlist_content(playlist_key: str, group: str, build: Aiogoogle.discover, aio: Aiogoogle) -> list[
+    str, list[str]]:
     res = await aio.as_api_key(build.playlistItems.list(part='snippet',
                                                         fields='items/snippet/resourceId/videoId,nextPageToken',
                                                         playlistId=playlist_key,
@@ -34,5 +35,6 @@ async def youtube_data_api_v3(playlist_key: str, group: str, build: Aiogoogle.di
 
 
 start_time = time.time()
-asyncio.run(google_api())
+music_video_list = asyncio.run(main())
+pprint.pprint(music_video_list)
 print(time.time() - start_time)
