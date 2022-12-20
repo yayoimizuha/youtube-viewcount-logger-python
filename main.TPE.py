ec4e6e5d4133151c7a7dbd9b9d297066e7c8a926
yayoimizuha <poko.peko.tomo@icloud.com>
2022-12-20 23:50:47 +0900

    update

--- main.TPE.py
+++ main.TPE.py
@@ -1,40 +1,24 @@
-from googleapiclient.discovery import build
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from os import getenv, getcwd, path, environ, cpu_count
-from const import playlists
-from pprint import pprint
+import aiohttp
+import asyncio
+from os import getenv
+from urllib import parse
+
+YTV3_ENDPOINT = "https://www.googleapis.com"
 
 API_KEY = getenv('YTV3_API_KEY', default='')
-
 if API_KEY == '':
     print('No API Key.')
     exit(-1)
-youtube = build(
-    'youtube',
-    'v3',
-    developerKey=API_KEY
-)
-
-print(playlists())
 
 
-def process_channel(artistName, playlistKey):
-    channelInfo = youtube.channels().list(
-        part='snippet,brandingSettings,statistics',
-        fields='items(snippet/thumbnails/high,'
-               'brandingSettings/image,'
-               'statistics(subscriberCount,videoCount,viewCount))',
-        id=youtube.playlistItems().list(part='snippet',
-                                        fields='items/snippet/channelId',
-                                        playlistId=playlistKey,
-                                        maxResults=1).execute()['items'][0]['snippet']['channelId']).execute()
-    pprint(channelInfo)
+def query_builder(resource_type: str,
+                  arg: dict,
+                  content_id: str,
+                  part: str,
+                  service_name: str = "youtube",
+                  version: str = "v3",
+                  key: str = API_KEY) -> str:
+    base_url: str = "/".join([YTV3_ENDPOINT, service_name, version, resource_type])
 
 
-thread_executor = ThreadPoolExecutor(max_workers=cpu_count())
-resp = list()
-for key, name, _ in playlists():
-    resp.append(thread_executor.submit(process_channel, name, key))
-
-for future in as_completed(resp):
-    print(future.result())
+print(query_builder(arg={"a": 1}, content_id="b", part="c", resource_type="d"))
