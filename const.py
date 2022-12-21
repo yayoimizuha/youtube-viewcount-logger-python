b08cfcfa6d12b24e49fc7874beb0b933345789f1
yayoimizuha <poko.peko.tomo@icloud.com>
2022-12-21 11:48:26 +0900

    update

--- const.py
+++ const.py
@@ -1,3 +1,7 @@
+from re import IGNORECASE, sub, MULTILINE
+from unicodedata import normalize
+
+
 def playlists():
     return [
         ['PLAAEA82D2950BC77D', 'モーニング娘。', True],
@@ -51,3 +55,50 @@ def playlists():
 #         ['PLeUX-FlHsb-tGpXYdlTS8rjjqCLxUB-eh', '鈴木愛理', True]
 #     ]
 #
+
+
+def trim_title(text, artist_name):
+    title_regex_one = r"[\(（]([a-zA-Z\s\[\]\'\"”””“\.…,\/　!！=。@’[°C]・:〜\-])*[\)）]"
+    title_regex_two = r"[\s|-][Promotion|Music|Video].*[\s|\-]*|Edit|カバー|[\s|-]*YouTube[\s|\-]*|ver|【セルフカヴァー】" \
+                      r"|Promotion|Music|Video"
+    title_regex_three = r"[\[\(（]([a-zA-Z\s”\[［\]］\.\/’\'\"&。:〜”“0-9\-=\?!×#~,♂　@（）])*[\]\)）]?"
+    title_regex_four = r'※.*'  # r"ショート|Short|short|Version|Ver.|Ver|バージョン|Dance|ダンス|リリック.*|"
+    if artist_name == '鞘師里保':
+        return sub(r'\(.*\)', '', text)
+    if artist_name == 'COVERS - One on One -':
+        return text
+    if artist_name == 'アップアップガールズ(仮)':
+        tmp = sub(r'【MUSIC VIDEO】|\([A-z\s\[\]\-！!].*\)|ミュージック|イメージ|ビデオ|\[.*?\]', '', text,
+                  IGNORECASE)
+        return sub(r'UP|GIRLS|kakko|KARI|MV|MUSIC|VIDEO|（MV）', '', tmp, IGNORECASE | MULTILINE)
+    if artist_name == 'シャ乱Q':
+        return sub(r'[v|(].*', '', normalize('NFKC', text))
+    if artist_name == 'つんく♂' or artist_name == '℃-ute' or artist_name == 'Berryz工房' or artist_name == 'Buono!' \
+            or artist_name == 'PINK CRES.' or artist_name == 'こぶしファクトリー' or artist_name == 'ブラザーズ5' \
+            or artist_name == '真野恵里菜' or artist_name == '鈴木愛理':
+        return sub(r'\(.*', '', normalize('NFKC', text))
+    if artist_name == 'Bitter & Sweet':
+        return sub(r'\(.*?\)', '', normalize('NFKC', text))
+    if artist_name == 'HANGRY&ANGRY':
+        return normalize('NFKC', text).replace('VIDEO CLIP', '')
+    if artist_name == 'Juice=Juice' or artist_name == 'アンジュルム' or artist_name == '高橋愛・田中れいな・夏焼雅' \
+            or artist_name == 'BEYOOOOONDS' or artist_name == 'モーニング娘。' or artist_name == 'OCHA NORMA':
+        return sub(r'\(.*?\)|\[.*?]|MV|Promotion|Edit', '', normalize('NFKC', text))
+    if artist_name == 'LoVendoЯ':
+        return sub(r'\(.*|\[.*', '', normalize('NFKC', text))
+    if artist_name == '小片リサ':
+        return sub(r'\-.*', '', normalize('NFKC', text))
+    if artist_name == 'ハロプロダンス部':
+        return sub(r'\(.*?\)', '', normalize('NFKC', text))
+
+    text = str(text).replace('(仮)', '@kari@')
+    text = str(text).replace('（仮）', '@kari@')
+    text = str(text).replace('℃-ute', '@cute@')
+    text = str(text).replace('°C-ute', '@cute@')
+    return_code = sub(title_regex_four, '',
+                      sub(title_regex_three, '',
+                          sub(title_regex_two, '',
+                              sub(title_regex_one, ' ', normalize('NFKC', text),
+                                  IGNORECASE), IGNORECASE | MULTILINE), IGNORECASE),
+                      IGNORECASE)
+    return return_code.replace('@kari@', '(仮)').replace('@cute@', '℃-ute')
--- main.TPE.py
+++ main.TPE.py
@@ -1,10 +1,10 @@
 from time import time
 from typing import Any
 from aiohttp import ClientSession
-from asyncio import gather, run
-from os import getenv, EX_CONFIG
+from asyncio import gather, run, Semaphore
+from os import getenv
 from urllib.parse import urlencode
-from const import playlists
+from const import playlists, trim_title
 from pprint import pprint
 
 YTV3_ENDPOINT = "https://www.googleapis.com/youtube/v3"
@@ -12,7 +12,7 @@ YTV3_ENDPOINT = "https://www.googleapis.com/youtube/v3"
 API_KEY = getenv('YTV3_API_KEY', default='')
 if API_KEY == '':
     print('No API Key.')
-    exit(EX_CONFIG)
+    exit(-1)
 
 
 def query_builder(resource_type: str,
@@ -23,13 +23,13 @@ def query_builder(resource_type: str,
     return f"{base_url}?{urlencode(arg)}"
 
 
-async def list_playlist(yt_key: str, artist_name: str, session: ClientSession) -> dict[Any, Any]:
+async def list_playlist(playlist_key: str, artist_name: str, session: ClientSession) -> dict[Any, Any]:
     playlist_item = list()
-    next_page_token: str = ''
+    next_page_token = str()
     while True:
         query = {"arg": {"part": 'snippet',
                          "fields": 'items/snippet/resourceId/videoId,nextPageToken',
-                         "playlistId": yt_key,
+                         "playlistId": playlist_key,
                          "maxResults": 50,
                          "pageToken": next_page_token
                          }, "resource_type": "playlistItems"}
@@ -42,14 +42,24 @@ async def list_playlist(yt_key: str, artist_name: str, session: ClientSession) -
     return {artist_name: [item['snippet']['resourceId']['videoId'] for item in playlist_item]}
 
 
+async def get_video_data(video_key: str, artist_name: str, session: ClientSession):
+    pass
+
+
 async def runner() -> None:
     start_time: float = time()
     sess = ClientSession(trust_env=True)
+    sem = Semaphore(value=300)
     video_list = await gather(
-        *[list_playlist(yt_key, artist_name, sess) for yt_key, artist_name, _ in playlists()], return_exceptions=True)
+        *[list_playlist(yt_key, artist_name, sess) for yt_key, artist_name, _ in playlists()],
+        return_exceptions=True)
     pprint(video_list)
     await sess.close()
     print(time() - start_time)
+    for item in video_list:
+        name = list(item.keys())[0]
+        print(name)
+        print(set(item[name]))
 
 
 run(runner())
