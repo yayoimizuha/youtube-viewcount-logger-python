3c63f0e152257db03001a8d9df16fe9d92f7eb18
yayoimizuha <poko.peko.tomo@icloud.com>
2023-01-19 00:22:18 +0900

    update

--- graph_gen.py
+++ graph_gen.py
@@ -1,10 +1,9 @@
 from os import path, getcwd
 from sqlite3 import connect
 from japanize_matplotlib import japanize
-from matplotlib import pyplot
-from pandas import read_sql, Int64Dtype, DataFrame, Series, concat,NA
+from matplotlib import pyplot, rcParams
+from pandas import read_sql, Int64Dtype, DataFrame, Series, concat, NA
 from datetime import date, timedelta
-from numpy import polyfit
 
 SQLITE_DATABASE = path.join(getcwd(), 'save.sqlite')
 
@@ -24,7 +23,8 @@ def frame_collector() -> dict[str:DataFrame]:
     connector = connect(SQLITE_DATABASE)
     cursor = connector.cursor()
     table_name = [name[0] for name in cursor.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()]
-    tables:dict[DataFrame] = {name: read_sql(f"SELECT * FROM {pack_comma(name)}", connector, index_col='index') for name in table_name}
+    tables: dict[DataFrame] = {name: read_sql(f"SELECT * FROM {pack_comma(name)}", connector, index_col='index') for
+                               name in table_name}
 
     for dataframe_key in tables.keys():
         column_list = tables[dataframe_key].columns.tolist()[1:]
@@ -38,8 +38,7 @@ def frame_collector() -> dict[str:DataFrame]:
         sparse_dataframe = DataFrame(columns=column_list, dtype=Int64Dtype())
         tables[dataframe_key] = concat(objs=[sparse_dataframe, tables[dataframe_key]], join='outer')
         tables[dataframe_key]: DataFrame = tables[dataframe_key].reindex(columns=column_list)
-        tables[dataframe_key]=tables[dataframe_key].replace(0,NA)
-        # tables[dataframe_key].iloc[:,1:] = tables[dataframe_key].iloc[:,1:].interpolate(axis=0,method='linear')
+        tables[dataframe_key] = tables[dataframe_key].replace(0, NA)
 
     return tables
 
@@ -50,13 +49,15 @@ for key, value in dataframes.items():
     value: DataFrame = value
     value.dropna(subset=['タイトル', value.columns[-1]], axis=0, how='any', inplace=True)
     value.sort_values(by=value.columns[-1], inplace=True, ascending=False)
-    if value.columns.__len__() < 3: continue
-    value.loc[:, 'today_displace'] = value.loc[:, value.columns[-1]] - value.loc[:, value.columns[-2]]
-    value = value[value['today_displace'] > value.iat[0, -1] / 15]
-    if value.index.__len__() > 27:
-        threshold = value.sort_values(by='today_displace', ascending=False).iloc[26, -1]
-        value = value[value['today_displace'] > threshold]
-    value = value.drop(columns=['today_displace'])
+    displacing = value.dropna(axis=1, how='all')
+    if displacing.columns.__len__() < 3: continue
+    displacing.loc[:, 'today_displace'] = \
+        displacing.loc[:, displacing.columns[-1]] - displacing.loc[:, displacing.columns[-2]]
+    displacing = displacing[displacing['today_displace'] > displacing.iat[0, -1] / 15]
+    if displacing.index.__len__() > 27:
+        threshold = displacing.sort_values(by='today_displace', ascending=False).iloc[26, -1]
+        displacing = displacing[displacing['today_displace'] > threshold]
+    value.drop(index=set(value.index) - set(displacing.index), inplace=True)
     value.set_index('タイトル', inplace=True)
     print(value)
     value = value.T
@@ -68,4 +69,6 @@ for key, value in dataframes.items():
     pyplot.xlabel('日付')
     pyplot.ylabel('再生回数')
     pyplot.title(key)
-    pyplot.show()
+    # pyplot.show()
+    pyplot.savefig(path.join(getcwd(), "graph", key + '.png'))
+    pyplot.close()
