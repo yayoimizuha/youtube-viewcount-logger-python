18678601fb642b0c5f37cb044e9666e7c998aef8
yayoimizuha <poko.peko.tomo@icloud.com>
2023-03-15 09:24:21 +0900

    change chrome to firefox

---
+++ firefox-install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2164
+cd /tmp/
+sudo apt install curl tar -y
+curl -sSL "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=ja" | tar -jxvf -
+sudo mv firefox /opt
