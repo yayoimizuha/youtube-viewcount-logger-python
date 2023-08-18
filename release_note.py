950c453092270fb38ff88d102714c40f882169d2
Tomokazu Katayama <poko.peko.tomo@icloud.com>
2023-08-19 08:15:08 +0900

    update tweet.py

--- release_note.py
+++ release_note.py
@@ -1,12 +1,12 @@
 from os.path import join
 from pprint import pprint
 from tweet import generate_txt
-from sys import stdout, __stdout__,argv
+from sys import stdout, __stdout__, argv
 from os import devnull, getcwd
 from urllib.parse import quote
 from pickle import load
 
-with open(file=join(getcwd(),'markdown.pickle'),mode='rb') as f:
+with open(file=join(getcwd(), 'markdown.pickle'), mode='rb') as f:
     markdown_table_dict = load(file=f)
 
 update_data = generate_txt()
@@ -17,8 +17,9 @@ for key, value in update_data.items():
     release_note_md += f'## {key}\n'
     release_note_md += value[0].split('\n', maxsplit=1)[1].replace('\n', '  \n') + '\n'
     release_note_md += f'  {markdown_table_dict[key]}  \n'
-    release_note_md += (f'![{key}の再生回数の推移](https://raw.githubusercontent.com/yayoimizuha/youtube-viewcount-logger'
-                        f'-python/{argv[1]}/graph/{quote(key)}.png)\n  \n')
+    release_note_md += (
+        f'![{key}の再生回数の推移](https://raw.githubusercontent.com/yayoimizuha/youtube-viewcount-logger'
+        f'-python/{argv[1]}/graph/{quote(key)}.png)\n  \n')
 
 with open(file=join(getcwd(), 'release_note.md'), mode='w') as f:
     f.write(release_note_md)
