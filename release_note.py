afc777a91a964f64fc1d6784a78aff123fef4e1f
yayoimizuha <poko.peko.tomo@icloud.com>
2023-08-18 17:19:59 +0900

    update

---
+++ release_note.py
@@ -0,0 +1,24 @@
+from os.path import join
+from pprint import pprint
+from tweet import generate_txt
+from sys import stdout, __stdout__
+from os import devnull, getcwd
+from urllib.parse import quote
+from pickle import load
+
+with open(file=join(getcwd(),'markdown.pickle'),mode='rb') as f:
+    markdown_table_dict = load(file=f)
+
+update_data = generate_txt()
+release_note_md = str()
+for key, value in update_data.items():
+    # print(key)
+    # print(value[0].split('\n',maxsplit=1)[1])
+    release_note_md += f'## {key}\n'
+    release_note_md += value[0].split('\n', maxsplit=1)[1].replace('\n', '  \n') + '\n'
+    release_note_md += f'  {markdown_table_dict[key]}  \n'
+    release_note_md += (f'![{key}の再生回数の推移](https://raw.githubusercontent.com/yayoimizuha/youtube-viewcount-logger'
+                        f'-python/master/graph/{quote(key)}.png)\n  \n')
+
+with open(file=join(getcwd(), 'release_note.md'), mode='w') as f:
+    f.write(release_note_md)
