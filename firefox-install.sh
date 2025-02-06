2b655ab0ec8d18937b9978b606e3ed7a1679ba2d
yayoimizuha <poko.peko.tomo@icloud.com>
2025-02-06 11:53:20 +0900

    set llvmlite version.

--- firefox-install.sh
+++ firefox-install.sh
@@ -2,5 +2,5 @@
 # shellcheck disable=SC2164
 cd /tmp/
 sudo apt install curl tar -y
-curl -sSL "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=ja" | tar -jxf -
+curl -sSL "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=ja" | tar -xf -
 sudo mv firefox /opt
